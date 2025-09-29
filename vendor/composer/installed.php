b2b8628ce5c33902e69264a52e06ae3dc39a3c0b
ZerolLka <masterdew4@gmail.com>
2025-09-29 23:25:36 +0300

    LR3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e2e1d062b90ae7e4fe4185c437c4f0c8918db184',
+        'name' => 'igor/guess-number',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'igor/guess-number' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e2e1d062b90ae7e4fe4185c437c4f0c8918db184',
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.6',
+            'version' => '0.12.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'reference' => 'f12b650d3738e471baed6dd47982d53c5c0ab1c3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
